81712399539aa2d6c92871aefcf5936cace04bdf
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-06-29 19:28:04 -0700

    1 passing data migration table up

---
+++ data/migrations/20220630021129_migration_name.js
@@ -0,0 +1,54 @@
+exports.up = async function(knex) {
+    await knex.schema
+      .createTable('projects', table => {
+          table.increments('project_id')
+          table.string('project_name').notNullable()
+          table.string('project_description', 500)
+          table.boolean('project_completed').defaultTo(0)
+      })
+      .createTable('resources', table =>{
+          table.increments('resource_id')
+          table.string('resource_name').notNullable().unique()
+          table.string('resource_description', 500)
+  
+      })
+      .createTable('tasks',table =>{
+          table.increments('task_id')
+          table.string('task_description', 500).notNullable()
+          table.string('task_notes', 500)
+          table.boolean('task_completed').defaultTo(0)
+          table.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+  
+  }) 
+      .createTable('project_resources', table => {
+          table.increments('project_resource_id')
+          table.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onDelete('restrict')
+          table.integer('resource_id')
+              .unsigned()
+              .notNullable()
+              .references('resource_id')
+              .inTable('resources')
+              .onDelete('CASCADE')
+  })
+  };
+  exports.down = async function(knex) {
+    return await knex.schema
+    
+    .dropTableIfExists('projects')
+
+    .dropTableIfExists('resources')
+
+    .dropTableIfExists('tasks')
+
+    .dropTableIfExists('project_resources')
+};
+
